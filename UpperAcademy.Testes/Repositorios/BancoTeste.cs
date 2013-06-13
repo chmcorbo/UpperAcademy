39b2cd3cd7f1dbee3bcf0ef1d656c732bf664232
Carlos Henrique Meireles Corbo <chmcorbo@gmail.com>
2013-06-13 00:43:21 -0300

    Criação do Banco
    Implementação de uma configuração que permite criar ou não o banco de
    dados.

---
+++ UpperAcademy.Testes/Repositorios/BancoTeste.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UpperAcademy.Persistence.nHibernate.Repositorio;
+using NUnit.Framework;
+
+namespace UpperAcademy.Testes.Repositorios
+{
+    [TestFixture]
+    public class BancoTeste
+    {
+        public BancoTeste()
+        {
+        }
+
+        [Test]
+        public void GerarBanco()
+        {
+            Assert.IsTrue(Banco.gerarBanco());
+        }
+
+    }
+}
---
+++ UpperAcademy.nHibernate/Repositorio/Banco.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UpperAcademy.Persistence.nHibernate.Repositorio
+{
+    public class Banco
+    {
+        public static bool gerarBanco()
+        {
+            try
+            {
+                FluentSessionFactory.AbrirSession(true);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
